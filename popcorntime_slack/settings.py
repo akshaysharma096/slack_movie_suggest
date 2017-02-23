ca94cfc4a3426655da88d75fc56f79fe897e9f78
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-02-23 21:26:09 +0530

    local_settings

--- popcorntime_slack/settings.py
+++ popcorntime_slack/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.postgres'
+    'django.contrib.postgres',
     'rest_framework',
     'api',
     'request_api'
