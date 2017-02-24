556d13a652e28dcae0744670cb79f457430ce11d
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-02-24 16:43:20 +0530

    Add allowed hots settings for django

--- popcorntime_slack/settings.py
+++ popcorntime_slack/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.154.183.26']
 
 # Application definition
 
