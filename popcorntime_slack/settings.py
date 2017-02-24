ad8f74f45b4386e6da5c680c6fc5f83e75c1cc02
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-02-25 01:22:23 +0530

    added allowed hots

--- popcorntime_slack/settings.py
+++ popcorntime_slack/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.56.152']
 
 # Application definition
 
