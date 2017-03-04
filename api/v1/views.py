87c4e44905b3b7bfce29f805901c93a67460413d
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-03-04 21:13:56 +0530

    Add csrf middleware for requests

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,4 +1,6 @@
 import ujson
+
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET
 
 from api.http_codes import http_codes
@@ -8,7 +10,7 @@ from request_api.popcorn_time import popcorn_time
 from slack_movie_suggest.settings import RANDOM_MOVIE
 
 
-
+@csrf_exempt
 @require_POST
 def get_movie(request, format=None):
     r = popcorn_time.get_request(RANDOM_MOVIE)
