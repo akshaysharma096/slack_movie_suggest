3f3da50c696c913ced127588d64f27f71cf93b19
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-03-04 20:03:50 +0530

    Removed csrf and other options

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,6 +1,4 @@
 import ujson
-
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_GET
 
 from api.http_codes import http_codes
@@ -10,7 +8,7 @@ from request_api.popcorn_time import popcorn_time
 from slack_movie_suggest.settings import RANDOM_MOVIE
 
 
-@csrf_exempt
+
 @require_POST
 def get_movie(request, format=None):
     r = popcorn_time.get_request(RANDOM_MOVIE)
