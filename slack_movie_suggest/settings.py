f3f4bbc44576f60cedc61f5669612d2e3dec35e7
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-03-05 16:35:35 +0530

    set allowed hosts

--- slack_movie_suggest/settings.py
+++ slack_movie_suggest/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.cueask.com/']
 
 # Application definition
 
