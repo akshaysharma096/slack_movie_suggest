836a46f692207fd3e17cb13a7500af977146da3b
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-03-07 00:26:33 +0530

    Typing

--- api/v1/webhooks/actions.py
+++ api/v1/webhooks/actions.py
@@ -8,11 +8,11 @@ from slack_movie_suggest.settings import FACEBOOK_WEBHOOK_TOKEN
 def send_typing(user_id):
     url = "https://graph.facebook.com/v2.6/me/messages?access_token=%s" % FACEBOOK_WEBHOOK_TOKEN
     response = {
-        'recipient': {
-            'id': user_id
+        "recipient": {
+            "id": user_id
         },
-        'sender_action': 'typing_on'
+        "sender_action": "typing_on"
     }
-    headers = {'content-type': 'application/json'}
+    headers = {'Content-Type': 'application/json'}
     print(response)
     requests.post(url, ujson.dumps(response), headers=headers)
